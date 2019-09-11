6c5e6ca6e46c51bb633f59f7f23a2c2520ac7d1d
ONevets <otraigusnevets@gmail.com>
2019-09-11 00:40:09 -0700

    Working number of table indicator

--- customer.js
+++ customer.js
@@ -25,17 +25,41 @@ app.use(express.static("public"));
 
 var tableSetupSchema = new mongoose.Schema({
   tableNumber: Number,
-  setupComplete: {default: false}
+  setupComplete: {type: Boolean, default: false}
+});
+
+var setupSchema = new mongoose.Schema({
+  nameOfRestaurant: String,
+  sizeTableS: Number,
+  sizeTableM: Number,
+  sizeTableL: Number,
+  totalTable: Number,
+  setupComplete: {
+    type: Boolean,
+    default: false
+  }
+});
+
+var reserveSchema = new mongoose.Schema({
+  tableNumber: Number,
+  sizeOfTable: String,
+  taken: {
+    type: Boolean,
+    default: false
+  }
 });
 
 //MODELS
 
-const tableSetup = mongoose.model("tableSetup", tableSetupSchema);
+const Setup = mongoose.model("Setup", setupSchema);
+const Reserve = mongoose.model("Reserve", reserveSchema);
+const TableSetup = mongoose.model("TableSetup", tableSetupSchema);
 
 //APP.ROUTE
+
 app.route("/")
   .get(function(req, res) {
-    Setup.findOne({
+    TableSetup.findOne({
       setupComplete: true
     }, function(err, resultSetup) {
       if (err) {
@@ -44,179 +68,43 @@ app.route("/")
       if (!resultSetup) {
         res.redirect("/setup");
       } else {
-        // Reserve.collection.drop();
-        Reserve.findOne({}, function(err, resultReserve) {
-          if (err) {
-            console.log(err);
-          }
-
-          if (!resultReserve) {
-            //getting the name of the restaurant
-            sizeTableS += resultSetup.sizeTableS;
-            sizeTableM += resultSetup.sizeTableM;
-            sizeTableL += resultSetup.sizeTableL;
-            totalTable = 1;
-
-            for (let i = 0; i < sizeTableS; i++) {
-              const reserve = new Reserve({
-                tableNumber: totalTable,
-                sizeOfTable: "S",
-                taken: false
-              });
-
-              totalTable += 1;
-              reserve.save();
-            }
-
-            for (let i = 0; i < sizeTableM; i++) {
-              const reserve = new Reserve({
-                tableNumber: totalTable,
-                sizeOfTable: "M",
-                taken: false
-              });
-
-              totalTable += 1;
-              reserve.save();
-            }
-
-            for (let i = 0; i < sizeTableL; i++) {
-              const reserve = new Reserve({
-                tableNumber: totalTable,
-                sizeOfTable: "L",
-                taken: false
-              });
-
-              totalTable += 1;
-              reserve.save();
-            }
-
-            //minus'd due to table array count
-            totalTable -= 1;
-            res.redirect("/reserve");
-
-          } else {
-            res.redirect("/reserve");
-          }
-
-
-        });
+        res.redirect("/menu");
       }
     });
   });
 
 app.route("/setup")
   .get(function(req, res) {
-    Setup.collection.drop();
-    res.render("setup.ejs");
-  })
-  .post(function(req, res) {
-    let setupTotalNumber = 0;
-
-    for (let i = 0; i < req.body.sizeTableS; i++) {
-      setupTotalNumber++;
-    }
-
-    for (let i = 0; i < req.body.sizeTableM; i++) {
-      setupTotalNumber++;
-    }
-
-    for (let i = 0; i < req.body.sizeTableL; i++) {
-      setupTotalNumber++;
-    }
-
-    const setup = new Setup({
-      nameOfRestaurant: req.body.nameOfRestaurant,
-      sizeTableS: req.body.sizeTableS,
-      sizeTableM: req.body.sizeTableM,
-      sizeTableL: req.body.sizeTableL,
-      totalTable: setupTotalNumber,
-      setupComplete: true
-    });
-
-    setup.save();
-
-    res.redirect("/");
-  });
-
-app.route("/reserve")
-  .get(function(req, res) {
-
-    //if havent setup yet then go to /setup
-    Setup.findOne({}, function(err, resultSetup) {
-      if (err) {
-        console.log(err);
-      }
-      if (!resultSetup) {
-        res.redirect("/setup");
-      } else {
-
-        Reserve.find({}, function(err, resultReserve) {
-          let arraySizeTableS = [];
-          let arraySizeTableM = [];
-          let arraySizeTableL = [];
-
-          if (err) {
-            console.log(err);
-          }
-
-          if (resultReserve) {
-            resultReserve.forEach(function(element) {
-              if (element.sizeOfTable === "S") {
-                arraySizeTableS.push(element.sizeOfTable);
-              } else if (element.sizeOfTable === "M") {
-                arraySizeTableM.push(element.sizeOfTable);
-              } else if (element.sizeOfTable === "L") {
-                arraySizeTableL.push(element.sizeOfTable);
-              }
-            });
-            console.log(arraySizeTableL);
-            console.log(arraySizeTableS);
-            console.log(arraySizeTableM);
-            res.render("reserve.ejs", {
-              nameOfRestaurant: resultSetup.nameOfRestaurant,
-              arraySizeTableS: arraySizeTableS,
-              arraySizeTableM: arraySizeTableM,
-              arraySizeTableL: arraySizeTableL,
-              totalTable: resultSetup.totalTable
-            });
-          }
-        });
-      }
-    });
-  });
-
-//booking the place
-app.route("/booked")
-  .get(function(req,res){
-    res.render("booked.ejs");
-  })
-  .post(function(req,res){
-    console.log(req.body);
-    tableNumber = parseInt(req.body.button);
-    console.log(tableNumber);
-    Reserve.findOne({tableNumber: tableNumber}, function(err, foundTable){
+    Setup.findOne({setupComplete: true}, function(err, resultFound){
       if(err){
         console.log(err);
       }
-      if(foundTable.taken === true){
-        res.redirect("/taken");
-      } else{
-        Reserve.updateOne({tableNumber: tableNumber }, {$set: {taken: true} }, function(err){
-          if(err){
-            console.log(err);
-          } else{
-            console.log("Updated");
-          }
-        });
-        res.redirect("/booked");
+      else{
+        // tableSetup.collection.drop();
+        let sizeTableS = resultFound.sizeTableS;
+        let sizeTableM = resultFound.sizeTableM;
+        let sizeTableL = resultFound.sizeTableL;
+        let totalTable = resultFound.totalTable;
+        res.render("customer/setup.ejs", {sizeTableS: sizeTableS, sizeTableM: sizeTableM, sizeTableL: sizeTableL, totalTable: totalTable});
       }
+
     });
 
+  })
+  .post(function(req, res) {
+    console.log(req.body);
+    let tableNumber = req.body.tableNumber;
+    const tableSetup = new TableSetup({
+      tableNumber: tableNumber,
+      setupComplete: true
+    });
+    tableSetup.save();
+    res.redirect("/");
   });
 
-app.route("/taken")
+app.route("/menu")
   .get(function(req,res){
-    res.render("taken.ejs");
+    res.render("customer/menu.ejs");
   });
 
 //APP.LISTEN
